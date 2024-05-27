5f1266b47923e71498fbd6046eb75dd5946b143e
Aziz Imranzade <azizimranzade@gmail.com>
2024-05-27 22:20:38 +0400

    robots.txt bug

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ["/studio"],
       },
     ],
-    sitemap: `${process.env.VERCEL_PROJECT_PRODUCTION_URL}/sitemap.xml`,
+    sitemap: `${process.env.NEXT_PUBLIC_SITE_BASE_URL}/sitemap.xml`,
   };
 }
